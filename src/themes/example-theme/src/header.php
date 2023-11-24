1f336cf12a0b74bb8087cfe91dbf06896907c75b
wackycoffee <webworldcommunities@gmail.com>
2023-11-24 12:28:20 -0500

    Starting examples

---
+++ src/themes/example-theme/src/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html class="no-js" <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+		<?php
+		wp_head();
+
+		get_template_part( 'components/example-head/index' );
+		?>
+	</head>
+
+	<body>
+	<?php
+		wp_body_open();
+	?>
