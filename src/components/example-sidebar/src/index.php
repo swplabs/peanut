93a215f86a46c238825b41b8e3447668af754772
wackycoffee <webworldcommunities@gmail.com>
2023-12-02 19:58:32 -0500

    More stylings for example sidebar

--- src/components/example-sidebar/src/index.php
+++ src/components/example-sidebar/src/index.php
@@ -5,7 +5,7 @@
       
       <div class="card-head">
           <h6 class="card-title">Release Features</h6>
-          <div class="button"> Close <i class="icon icon-close"></i></div>
+          <div class="button">Close <i class="icon icon-close"></i></div>
       </div>
 
       <div class="card-content">
@@ -21,7 +21,7 @@
                       <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/example-icon.jpg' ) ); ?>" />
                       <div>
                         <div><span>Macbook Air - 8gb</span></div>
-                        <span>14K Item Sold</span>
+                        <span class="text-gray">14K Item Sold</span>
                       </div>
                     </div>
                     <div class="table-list-col">
@@ -33,7 +33,7 @@
                       <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/example-icon.jpg' ) ); ?>" />
                       <div>
                         <div><span>Macbook Air - 8gb</span></div>
-                        <span>14K Item Sold</span>
+                        <span class="text-gray">14K Item Sold</span>
                       </div>
                     </div>
                     <div class="table-list-col">
@@ -45,7 +45,7 @@
                       <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/example-icon.jpg' ) ); ?>" />
                       <div>
                         <div><span>Macbook Air - 8gb</span></div>
-                        <span>14K Item Sold</span>
+                        <span class="text-gray">14K Item Sold</span>
                       </div>
                     </div>
                     <div class="table-list-col">
@@ -57,7 +57,7 @@
                       <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/example-icon.jpg' ) ); ?>" />
                       <div>
                         <div><span>Macbook Air - 8gb</span></div>
-                        <span>14K Item Sold</span>
+                        <span class="text-gray">14K Item Sold</span>
                       </div>
                     </div>
                     <div class="table-list-col">
@@ -69,7 +69,7 @@
                       <img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/example-icon.jpg' ) ); ?>" />
                       <div>
                         <div><span>Macbook Air - 8gb</span></div>
-                        <span>14K Item Sold</span>
+                        <span class="text-gray">14K Item Sold</span>
                       </div>
                     </div>
                     <div class="table-list-col">
--- src/components/example-sidebar/src/view.js
+++ src/components/example-sidebar/src/view.js
@@ -1,3 +1,9 @@
-module.exports = async (instance, data) => {
-  console.log(instance, data);
+module.exports = async (instance) => {
+  const cards = instance.querySelectorAll('.card');
+  cards.forEach((card) => {
+    const closeButton = card.querySelector('.card-head .button');
+    closeButton.onclick = () => {
+      card.remove();
+    };
+  });
 };
